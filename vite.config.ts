6f5385872f0d2b52e83d31f5473d8ecd35981566
Matt Chan <58923400+multimattia@users.noreply.github.com>
2026-02-16 14:20:52 +0800

    Add project files

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { defineConfig } from 'vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
+
+export default defineConfig({
+  server: {
+    port: 10000,
+    strictPort: true,
+  },
+  plugins: [
+    nitro(),
+    tailwindcss(),
+    tsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    tanstackStart(),
+    viteReact(),
+  ],
+})
